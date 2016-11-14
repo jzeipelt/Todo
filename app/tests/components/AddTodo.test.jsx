5632fec0ed7b575a55403ccb8807ef652a492dd2
Jörg Zeipelt <Jrg Zeipelt>
2016-11-14 19:11:45 +0100

    Add AddTodo and SearchTodo

---
+++ app/tests/components/AddTodo.test.jsx
@@ -0,0 +1,37 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require('jQuery');
+
+var AddTodo = require('AddTodo');
+
+describe('AddTodo', () => {
+  it('should exist', () => {
+    expect(AddTodo).toExist();
+  });
+
+  it('should call onAddTodo if text is entered', () => {
+    var todoText = 'ABC';
+    var spy = expect.createSpy();
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(addTodo));
+
+    addTodo.refs.todo.value = todoText;
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toHaveBeenCalledWith(todoText);
+  });
+
+  it('should NOT call onAddTodo if nothing is entered', () => {
+    var todoText = '';
+    var spy = expect.createSpy();
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(addTodo));
+
+    addTodo.refs.todo.value = todoText;
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toNotHaveBeenCalled();
+  });
+});
