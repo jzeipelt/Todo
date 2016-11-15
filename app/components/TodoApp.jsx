0b1a7a3f24fc576c19c7f5e2cc984705285e1c2f
Jörg Zeipelt <Jrg Zeipelt>
2016-11-15 14:48:22 +0100

    Add toggle support

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -12,11 +12,13 @@ var TodoApp = React.createClass({
       todos: [
         {
           id: uuid(),
-          text: 'Mit dem Hund Gassi gehen'
+          text: 'Mit dem Hund Gassi gehen',
+          completed: false
         },
         {
           id: uuid(),
-          text: 'Aufräumen'
+          text: 'Aufräumen',
+          completed: true
         }
       ]
     };
@@ -28,10 +30,21 @@ var TodoApp = React.createClass({
         ...this.state.todos,
         {
           id: uuid(),
-          text: text
+          text: text,
+          completed: false
         }
       ]
     });
+  },
+
+  handleToggle: function (id) {
+    var updatedTodos = this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    this.setState({todos: updatedTodos});
   },
 
   handleSearch: function (showCompleted, searchText) {
@@ -46,7 +59,7 @@ var TodoApp = React.createClass({
     return (
       <div>
         <TodoSearch onSearch={this.handleSearch}/>
-        <TodoList todos={todos}/>
+        <TodoList todos={todos} onToggle={this.handleToggle}/>
         <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
     );
