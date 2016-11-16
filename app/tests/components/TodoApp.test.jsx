a7b8d5d48f78c63861dec2f02919d5da05b1b270
Jörg Zeipelt <Jrg Zeipelt>
2016-11-16 11:42:12 +0100

    Add completed and created timestamps

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -14,23 +14,42 @@ describe('TodoApp', () => {
   it('should add todo to the todos state on handleAddTodo', () => {
     var todoText = 'Test text';
     var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
-
     todoApp.setState({todos: []});
     todoApp.handleAddTodo(todoText);
-
     expect(todoApp.state.todos[0].text).toBe(todoText);
+    expect(todoApp.state.todos[0].createdAt).toBeA('number');
   });
 
-  it('should toggle completed value when handleToggle called', () => {
+  it('should toggle incomplete todo when handleToggle called', () => {
     var todoData = {
       id: 11,
       text: 'Test',
-      completed: false
+      completed: false,
+      createdAt: 0,
+      completedAt: undefined
     };
     var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
     todoApp.setState({todos: [todoData]});
     expect(todoApp.state.todos[0].completed).toBe(false);
     todoApp.handleToggle(11);
     expect(todoApp.state.todos[0].completed).toBe(true);
+    expect(todoApp.state.todos[0].completedAt).toBeA('number');
+  });
+
+  it('should toggle complete todo when handleToggle called', () => {
+    var todoData = {
+      id: 12,
+      text: 'Test2',
+      completed: true,
+      createdAt: 0,
+      completedAt: 1
+    };
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({todos: [todoData]});
+    expect(todoApp.state.todos[0].completed).toBe(true);
+    expect(todoApp.state.todos[0].completedAt).toBeA('number');
+    todoApp.handleToggle(12);
+    expect(todoApp.state.todos[0].completed).toBe(false);
+    expect(todoApp.state.todos[0].completedAt).toBe(undefined);
   });
 });
